5fb3282a1e622466df16b0d499d963001b4c36dd
Ethan-S-Dev <ethan380s@gmail.com>
2023-09-15 16:07:28 +0300

    cleaner build

--- build_tests.bat
+++ build_tests.bat
@@ -1,15 +1,18 @@
+:: Execute the previous batch file to build the library
+@call build.bat
+@echo Library built successfully!
+
 @echo off
 
-:: Execute the previous batch file to build the library
-call build.bat
-
 :: Compile the main C file into an object file in the bin folder
-clang -c ./tests/list_tests.c -o bin\list_tests.o
+clang -c ./tests/list_tests.c -o ./bin/list_tests.o
+echo List tests compiled successfully!
 
 :: Link the object file with the static library to create the executable
-clang -v bin\list_tests.o -L./bin -l./bin/collections.lib -o bin\list_tests.exe
+clang ./bin/list_tests.o -L./bin -l./bin/collections.lib -o ./bin/list_tests.exe
+echo List tests built successfully!
 
 :: Optionally, delete the temporary object files from the bin folder
-:: del bin\list_tests.o
+del bin\list_tests.o
 
 @echo on
